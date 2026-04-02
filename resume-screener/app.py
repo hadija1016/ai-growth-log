8d6d47d82909a9a8b3a56b09103ede18552f585c
hadija1016 <hadijabinteatif16.com>
2026-04-03 02:05:34 +0500

    RESUME -SCREENER WITH AI SCORING COMPLETED

--- resume-screener/app.py
+++ resume-screener/app.py
@@ -61,5 +61,28 @@ if uploaded_file is not None:
     loader = PyPDFLoader("temp.pdf")
     docs=loader.load()
     RESUME_TEXT="\n".join([doc.page_content for doc in docs])
-    st.write(RESUME_TEXT)
-    
+    job_description=st.text_area("Paste the job description here",placeholder="""Enter the  full job description here, Tip: Include as much detail as possible:
+- Role_title (e.g., Data Scientist, Backend Engineer)
+- Required skills (e.g., Python, SQL, AWS)
+- Responsibilities
+- Experience level (e.g., 2+ years)
+- Tools/technologies
+- Any preferred qualifications
+
+You can copy this directly from LinkedIn, company website, or job posting.
+"""
+)
+    if st.button("Screen Resume"):
+       prompt = f"""You are a resume screener. Your task is to evaluate the candidate's resume against the provided job
+       resume={RESUME_TEXT}
+       job_description={job_description}
+       Please provide a score from 1 to 10 based on how well the resume matches the job description, along with a brief explanation for the score.
+       """
+       response = client.chat.completions.create(
+    model="openai/gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+)
+       result = response.choices[0].message.content
+       st.write(result)
